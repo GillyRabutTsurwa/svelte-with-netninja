36ac6a4ed90a91ba74976aa94763812651907a50
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-19 16:21:16 -0500

    part 16 - forms part 2: checkboxes and select/option

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,8 +2,16 @@
   let name;
   let beltColour;
   let age;
+  // declaring all these variables is not efficient
+  // let fighting = false;
+  // let sneaking = false;
+  // let running = false;
+
+  let skills = [];
+
   const handleSubmit = () => {
-    console.log(name, beltColour, age);
+    // console.log(name, beltColour, age, fighting, sneaking, running);
+    console.log(name, beltColour, age, skills);
   };
 </script>
 
@@ -18,19 +26,41 @@
 <form on:submit|preventDefault={handleSubmit}>
   <div class="inputs">
     <input type="text" placeholder="Name" bind:value={name} />
-    <input type="text" placeholder="Belt Colour" bind:value={beltColour} />
     <input type="number" placeholder="Age" bind:value={age} />
     <!-- NEW: CHECKBOXES -->
     <label for="">Skills</label>
-    <input type="checkbox" checked={true} id="" />
+    <!-- NOTE: how to check if a checkbox is checked. 
+    We can use the checked attribute
+     -->
+    <!-- THis method below is not efficient, especially if we have a lot of variables which we want to check the truth or false values. The Alternative is below this commented out code. -->
+    <!-- <input type="checkbox" bind:checked={fighting} />
     Fighting
     <br />
-    <input type="checkbox" name="" id="" />
+    <input type="checkbox" bind:checked={sneaking} />
     Sneaking
     <br />
-    <input type="checkbox" name="" id="" />
+    <input type="checkbox" bind:checked={running} /> 
+    Running 
+    <br /> -->
+    <input type="checkbox" bind:group={skills} value="fighting" />
+    Fighting
+    <br />
+    <input type="checkbox" bind:group={skills} value="sneaking" />
+    Sneaking
+    <br />
+    <input type="checkbox" bind:group={skills} value="running" />
     Running
     <br />
+    <label>Belt Colour:</label>
+    <!-- NOTE: Cannot explain what is going on here succintly, but if we pick the black option for example, then beltColour = black. 
+    So I think how it works is that we bind the our beltColour variable to our select element and its option children will determine the value of beltColour (upon which one is selected) based on their values... j'espère que cette deuxième partie fait du sens.
+     -->
+    <select bind:value={beltColour}>
+      <option value="black">black</option>
+      <option value="orange">orange</option>
+      <option value="brown">brown</option>
+      <option value="white">white</option>
+    </select>
   </div>
   <button>Add Person</button>
 </form>
