da4d0ce28b4b137a5a51c4321fe1a7530830789e
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-19 15:42:21 -0500

    modified AddPerson Component

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -20,6 +20,17 @@
     <input type="text" placeholder="Name" bind:value={name} />
     <input type="text" placeholder="Belt Colour" bind:value={beltColour} />
     <input type="number" placeholder="Age" bind:value={age} />
+    <!-- NEW: CHECKBOXES -->
+    <label for="">Skills</label>
+    <input type="checkbox" checked={true} id="" />
+    Fighting
+    <br />
+    <input type="checkbox" name="" id="" />
+    Sneaking
+    <br />
+    <input type="checkbox" name="" id="" />
+    Running
+    <br />
   </div>
   <button>Add Person</button>
 </form>
