8a2b0d6ede3c9dec350fa7d71acc65f578e0de24
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-07-31 22:19:43 -0500

    Perfecc. dynamic components using reactive values instead of the function I used earlier. Looks much cleaner and is probably more performant

--- src/App.svelte
+++ src/App.svelte
@@ -17,15 +17,21 @@
 
   //NEW: changing the value of component (which I'm led to believe is a reserved svelte keyword) to render appropriate component.
   let component;
-  function changeCompo() {
-    // activeItem === items[0] ? (component = PollList) : (component = PollForm);
-    // The above code works but I will switch items[0] with "Current Polls",
-    // I do this so that it looks for current polls despite of where it is in the array... hypothetically speaking
-    activeItem === "Current Polls"
-      ? (component = PollList)
-      : (component = PollForm);
-    return component;
-  }
+  // function changeCompo() {
+  //   // activeItem === items[0] ? (component = PollList) : (component = PollForm);
+  //   // The above code works but I will switch items[0] with "Current Polls",
+  //   // I do this so that it looks for current polls despite of where it is in the array... hypothetically speaking
+  //   activeItem === "Current Polls"
+  //     ? (component = PollList)
+  //     : (component = PollForm);
+  //   return component;
+  // }
+
+  //NOTE: Even better than the above. Instead of making a function, Je vais utiliser les valeurs reactives. Equivalent of computed properties in Vue.
+  // I wills still keep code above for reference.
+  $: activeItem === "Current Polls"
+    ? (component = PollList)
+    : (component = PollForm);
 </script>
 
 <style>
@@ -42,6 +48,6 @@
     activeItemProp={activeItem}
     on:tabChange={changeTab} />
   <!-- NEW: Dynamic component to render appropriate component -->
-  <svelte:component this={changeCompo()} />
+  <svelte:component this={component} />
   <Pied />
 </main>
