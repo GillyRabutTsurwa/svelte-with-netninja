b7f0c847350a100ebdc3bbaf22ebb8ad4935984f
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-09-08 18:20:46 -0500

    after a hiatus. I back again lesgooo

--- src/App.svelte
+++ src/App.svelte
@@ -24,9 +24,11 @@
   //NEW:
   const handleAdd = e => {
     console.log(e.detail);
-    const poll = e.detail;
-    polls = [poll, ...polls];
+    const newPoll = e.detail;
+    polls = [newPoll, ...polls]; //Using the spread operator. Niceee.
     console.log(polls);
+    // Ca va switcher vers le current polls juste apres avoir soumis notre form.
+    activeItem = "Current Polls";
   };
 
   // function that runs when event emisson from child to parent is called
@@ -50,8 +52,6 @@
 
 <main>
   <Tete />
-  <h1>Arrêté à 5:04</h1>
-  <p>Mais je vais probablement redémarrer la vidéo</p>
   <Tabs
     itemsProp={items}
     activeItemProp={activeItem}
@@ -59,6 +59,7 @@
   <!-- * NEWIMPORTANTNOTE: Au début, je pensais qu'on pouvais pas déclarer un évenement transferé (dispatched) d'enfant au parent si le parent déclare ce compenent dynamiquement (parmi les autres). Mais on peut le faire (comme j'ai fais dessous avec on:add). C'est parce que Svelte sait associer l'evenement déclaré avec le component enfant qui le transfère (l'evenement).
   C'est à dire. Cette déclaration ci-dessous (on:add) pour écouter l'evenement venant d'un component enfant est associée avec le component PollForm parce que c'est ce component qui transfere l'evenement. Ça, c'est super.
   QUESTION: VueJS et React peuvent-elle faire la même chose ? -->
-  <svelte:component this={component} on:add={handleAdd} />
+  <!-- Ignore this warning: <PollForm> was created with unknown prop 'pollsProp'. for now. Je viens de verifier la structure de l'application et ca c'est pas le cas. -->
+  <svelte:component this={component} on:add={handleAdd} pollsProp={polls} />
   <Pied />
 </main>
--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,17 @@
 <script>
-
+  export let pollsProp = [];
 </script>
 
 <style>
-
+  .poll-list {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+  }
 </style>
 
-<main>
-  <h1>Soon to be PollList Component</h1>
-</main>
+<div class="poll-list">
+  {#each pollsProp as currentPoll (currentPoll.id)}
+    <div>{currentPoll.question}</div>
+  {/each}
+</div>
