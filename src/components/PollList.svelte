586443278a380692c0df3b5bf4fe92a6fdbf107c
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-23 10:20:38 -0500

    unsubscribing from store method 2

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,26 +1,14 @@
 <script>
-  //NEW: lifecycle hooks, unlike in Vue, need to be imported
-  import { onMount, onDestroy } from "svelte";
+  /** NEWIMPORTANT
+   * For the alternative way of unsubscribing, instead of making a variable and assigning our data to it, we refer to the store directly as our "variable" by using the ${storeName} syntaxe.
+   *
+   * Therefore, the lifecycle hooks and the store.subscribe() are no longer needed. Also, the variable used to hold the store data - which in our case was the polls array - is no longer needed.
+   *
+   * I still don't know which method I like best. Cette syntaxe est plus propre, mais l'autre est plus explicit. Néanmoins, il me faut savoir utiliser les deux.
+   *
+   */
   import PollStore from "../store/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
-  export let pollsProp = [];
-
-  // unsubscribe from store. by putting variable. Will come back to this branch and write better notes
-  const callToUnsub = PollStore.subscribe(data => {
-    pollsProp = data;
-    console.log(data);
-  });
-
-  //NEW: lifecycle hooks. more info on theme here https://svelte.dev/tutorial/onmount
-
-  onMount(() => {
-    console.log("PollList component est monté");
-  });
-
-  onDestroy(() => {
-    console.log("PollList component est enlevé de DOM");
-    callToUnsub();
-  });
 </script>
 
 <style>
@@ -33,10 +21,8 @@
 </style>
 
 <div class="poll-list">
-  {#each pollsProp as currentPoll (currentPoll.id)}
+  {#each $PollStore as currentPoll (currentPoll.id)}
     <div>
-      <!-- NEWIMPORTANT: En ne définant une fonctionne pour on:vote, cet evenement est transferer a son parent. Ce que l'on veut.  -->
-      <!-- Ce composant est le liason de transferer des données spécifiques de son componsant enfant vers son composant parent. Car en peut pas faire directement (on n'utilise pas encore le store) -->
       <PollDetails singlePollProp={currentPoll} on:vote />
     </div>
   {/each}
