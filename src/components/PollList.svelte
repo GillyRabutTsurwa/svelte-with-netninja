5e8c83e33e8ea3d0691e06964fed53748b622b5b
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-23 15:58:31 -0500

    okay satisfied with the notes

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -10,7 +10,7 @@
    * Everytime the data in our store changes - this is coming later - {storeName}.subscribe is called. It is also called everytime the component wherewhich the store is being used is mounted. Doing this without unsubscribing from the store when the component is destroyed is not good for memory. So it is good practise to unsubscribe from the store each time it is not in use (when the component is destroyed for example).
    *
    * To do this, we assign our subscribe method a variable, and invoke this method in our onDestroy life-cycle hook. So, in our case, callToUnsub is assigned with our subscription code, and we call this function in our onDestroy lifecycle hook: callToUnsub().
-   * 
+   *
    * QUESTION: Hold up, is callToUnsub an IIFE?
    *
    */
@@ -29,8 +29,7 @@
   onDestroy(() => {
     console.log("PollList component est enlevé de DOM");
     callToUnsub();
-    console.log(callToUnsub.);
-    console.log(callToUnsub());
+    console.log(callToUnsub); // Curiosité
   });
 </script>
 
